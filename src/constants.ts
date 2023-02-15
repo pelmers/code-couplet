6340af25d7fedd02fdf1c9e1594331415bff5641
Peter Elmers <peter.elmers@yahoo.com>
2023-02-15 14:15:00 +0100

    update debug const based on runtime

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const PROJECT_NAME = "code-couplet";
 export const SCHEMA_VERSION = 1;
 
-export const DEBUG = true;
+export const DEBUG = process.execArgv.some((arg) => arg.includes("inspect"));
